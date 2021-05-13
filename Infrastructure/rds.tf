eadb76d9d78d733d93c1b97bea0b1ad00c691145
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-14 08:49:21 +0900

    とりあえずRDS構成 ※Route53の設定を先にやりたいので色々封印した状態

---
+++ Infrastructure/rds.tf
@@ -0,0 +1,49 @@
+variable database_password {}
+
+resource "aws_db_instance" "main" {
+    name                 = "SimpleSocialGame"
+    allocated_storage    = 10
+    engine               = "mysql"
+    engine_version       = "5.7"
+    instance_class       = "db.t3.micro"
+    username             = "develop"
+    password             = var.database_password
+    parameter_group_name = "default.mysql5.7"
+    skip_final_snapshot  = true
+    publicly_accessible  = true
+    db_subnet_group_name = aws_db_subnet_group.db_subnet.name
+ #   vpc_security_group_ids = [aws_security_group.db_sg.id]
+}
+
+resource "aws_db_subnet_group" "db_subnet" {
+  name       = "simple_social_game_db_subnet_group"
+  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id]
+}
+
+// ECS構築するまで封印
+/*
+resource "aws_security_group" "db_sg" {
+    name = "SimpleSocialGame_RDB"
+    description = "RDB"
+    vpc_id = aws_vpc.main.id
+
+    ingress {
+        from_port        = 3306
+        to_port          = 3306
+        protocol         = "TCP"
+        security_groups  = [aws_security_group.ecs_instance_sg.id]
+    }
+
+    egress {
+        from_port        = 3306
+        to_port          = 3306
+        protocol         = "TCP"
+        security_groups  = [aws_security_group.ecs_instance_sg.id]
+    }
+}
+*/
+
+resource "aws_db_subnet_group" "db_inst_subnet_group" {
+  name       = "simple_social_game"
+  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id]
+}
