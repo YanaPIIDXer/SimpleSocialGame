a90d7c3db0e1e9f6e97787802408873762c39ee0
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-14 18:20:32 +0900

    持ち石表示

---
+++ Client/SimpleSocialGame/Assets/Scripts/UI/StoneText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using UniRx;
+using Game.User;
+
+namespace Game.UI
+{
+    /// <summary>
+    /// 持ち石表示
+    /// </summary>
+    public class StoneText : MonoBehaviour
+    {
+        /// <summary>
+        /// 表示テキスト
+        /// </summary>
+        private Text DisplayText = null;
+
+        void Awake()
+        {
+            DisplayText = GetComponent<Text>();
+            UserInfo.Stone.Subscribe((Value) => DisplayText.text = string.Format("{0}石", Value));
+        }
+    }
+}
