ff6d7e034c4c7a4d4c9631a70954fac6f565a9c3
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-14 18:36:50 +0900

    エキスパンション管理クラス

---
+++ Client/SimpleSocialGame/Assets/Scripts/Expansion/ExpansionManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Expansion
+{
+    [Serializable]
+    public struct ExpansionData
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int id;
+
+        /// <summary>
+        /// 名前
+        /// </summary>
+        public string name;
+
+        /// <summary>
+        /// コスト
+        /// </summary>
+        public int cost;
+    }
+
+    /// <summary>
+    /// エキスパンション管理
+    /// </summary>
+    public class ExpansionManager
+    {
+        /// <summary>
+        /// エキスパンションリスト
+        /// </summary>
+        private List<ExpansionData> Expansions = new List<ExpansionData>();
+
+        /// <summary>
+        /// 追加
+        /// </summary>
+        /// <param name="Data">データ</param>
+        public void Add(ExpansionData Data)
+        {
+            Expansions.Add(Data);
+        }
+
+        /// <summary>
+        /// 全取得
+        /// </summary>
+        /// <returns>全てのデータ</returns>
+        public ExpansionData[] GetAll()
+        {
+            return Expansions.ToArray();
+        }
+
+        #region Singleton
+        public static ExpansionManager Instance { get { return _Instance; } }
+        private static ExpansionManager _Instance = new ExpansionManager();
+        #endregion
+    }
+
+}
