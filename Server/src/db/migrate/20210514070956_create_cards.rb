a8ae5ce5757a5d061f37b7abb52670e9f16954f0
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-14 16:22:15 +0900

    エキスパンションＩＤのカラム追加

--- Server/src/db/migrate/20210514070956_create_cards.rb
+++ Server/src/db/migrate/20210514070956_create_cards.rb
@@ -1,6 +1,7 @@
 class CreateCards < ActiveRecord::Migration[5.2]
   def change
     create_table :cards do |t|
+      t.integer :expansion_id, not_null: true
       t.string :asset_name, not_null: true
       t.timestamps
     end
--- Server/src/db/schema.rb
+++ Server/src/db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_05_14_070956) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "expansion_id"
     t.string "asset_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
