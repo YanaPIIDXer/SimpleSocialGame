f65558db3b49e9a79259d28da9b6009ed537249b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-14 14:12:18 +0900

    とりあえずタイトル画面のUIを配置

---
+++ Client/SimpleSocialGame/Assets/Scripts/UI/LoginForm.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Game.UI
+{
+    /// <summary>
+    /// ログインフォーム
+    /// </summary>
+    public class LoginForm : MonoBehaviour
+    {
+        /// <summary>
+        /// 名前入力欄
+        /// </summary>
+        [SerializeField]
+        private InputField NameInput = null;
+
+        /// <summary>
+        /// ログインボタン
+        /// </summary>
+        [SerializeField]
+        private Button LoginButton = null;
+    }
+}
