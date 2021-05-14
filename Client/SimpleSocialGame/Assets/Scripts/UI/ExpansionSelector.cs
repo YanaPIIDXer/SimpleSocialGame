fdf895f6065026d17e5fe7407b730a578d463e15
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-14 20:02:23 +0900

    とりあえずリストから選択してボタンを押すまで

---
+++ Client/SimpleSocialGame/Assets/Scripts/UI/ExpansionSelector.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Game.API;
+using Game.Expansion;
+
+namespace Game.UI
+{
+    /// <summary>
+    /// エキスパンション選択
+    /// </summary>
+    public class ExpansionSelector : MonoBehaviour
+    {
+        /// <summary>
+        /// エキスパンションリスト
+        /// </summary>
+        private ExpansionData[] Expansions = null;
+
+        /// <summary>
+        /// エキスパンション選択リスト
+        /// </summary>
+        [SerializeField]
+        private Dropdown ExpansionDropdown = null;
+
+        /// <summary>
+        /// 引くボタン
+        /// </summary>
+        [SerializeField]
+        private Button DrawButton = null;
+
+        void Awake()
+        {
+            Expansions = ExpansionManager.Instance.GetAll();
+            foreach (var Expansion in Expansions)
+            {
+                ExpansionDropdown.options.Add(new Dropdown.OptionData { text = Expansion.name });
+            }
+            ExpansionDropdown.value = 0;
+
+            DrawButton.onClick.AddListener(OnClickDrawButton);
+        }
+
+        /// <summary>
+        /// 引くボタンが押された
+        /// </summary>
+        private void OnClickDrawButton()
+        {
+            var Expansion = Expansions[ExpansionDropdown.value];
+            Debug.Log(Expansion.name);
+        }
+    }
+}
