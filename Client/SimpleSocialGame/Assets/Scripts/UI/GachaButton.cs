12491077d58c6b79f51262aac9934852989e6e97
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-14 18:28:58 +0900

    ガチャシーンを作成し、遷移できるようにした

---
+++ Client/SimpleSocialGame/Assets/Scripts/UI/GachaButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace Game.UI
+{
+    /// <summary>
+    /// ガチャシーン遷移ボタン
+    /// </summary>
+    public class GachaButton : MonoBehaviour
+    {
+        /// <summary>
+        /// シーン遷移ボタン
+        /// </summary>
+        private Button SceneMoveButton = null;
+
+        void Awake()
+        {
+            SceneMoveButton = GetComponent<Button>();
+            SceneMoveButton.onClick.AddListener(() => SceneManager.LoadScene("Gacha"));
+        }
+    }
+}
