8a462a8d8ff22b066b3e587460d108c65cf8e7c5
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-05-14 16:29:51 +0900

    エキスパンションのリストを返すようにした

--- Server/src/app/controllers/expansions_controller.rb
+++ Server/src/app/controllers/expansions_controller.rb
@@ -1,5 +1,10 @@
 class ExpansionsController < ApplicationController
     def index
-        render :json, {}
+        list = Expansion.all()
+        result = []
+        for expansion in list do
+            result.push(expansion.bundle_name)
+        end
+        render json: result.to_json()
     end
 end
